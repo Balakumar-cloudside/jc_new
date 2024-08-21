48cede18534e64bf99ddfbedb08694d73ed077e8
Balakumar-cloudside <119562666+Balakumar-cloudside@users.noreply.github.com>
2024-08-21 18:12:15 +0530

    Create stg_annual_enterprise_survey01.sql

---
+++ models/example/staging/stg_annual_enterprise_survey01.sql
@@ -0,0 +1,14 @@
+WITH
+survey AS (
+SELECT * FROM `graphite-dynamo-432313-f5.dbt_db_bala.annual-enterprise-survey02`),
+
+ae AS ( 
+SELECT 
+    id,
+    value,
+    Industry_aggregation_NZSIOC,
+    Variable_category
+FROM survey
+)
+
+SELECT * FROM ae
